7824dc1006c548b9eaab027308a9043871fb7141
Akram Izadeen <akramizadeen99@gmail.com>
2024-04-29 19:56:22 +0530

    App Updated for getWorkouts API Fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 import Form from './components/Form';
 import Workout from './components/Workout';
-import wImage from './assets/image.png'
+import wImage from './assets/image.png';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [workouts, setWorkouts] = useState([])
+
+  async function getWorkouts() {
+    const data = await fetch('http://localhost:4000/api/workouts', {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    })
+    const fetchedData = await data.json()
+    setWorkouts(fetchedData)
+  }
+
+  useEffect(() => {
+    getWorkouts()
+  }, [])
+
   return (
     <div className='App'>
       <h1>GET <span>FIT</span></h1>
@@ -15,11 +34,9 @@ function App() {
       </div>
       <div className='workout-list'>
         <h2>Workouts</h2>
-        <Workout title='Back Bench' loads='10' reps='8' />
-        <Workout title='Back Bench' loads='10' reps='8' />
-        <Workout title='Back Bench' loads='10' reps='8' />
-        <Workout title='Back Bench' loads='10' reps='8' />
-        <Workout title='Back Bench' loads='10' reps='8' />
+        {workouts.map((workout) => (
+          <Workout title={workout.title} loads={workout.load} reps={workout.reps} />
+        ))}
       </div>
     </div>
   );
